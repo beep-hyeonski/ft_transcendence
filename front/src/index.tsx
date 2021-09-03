4cd2627823b7e518ee3a1aa7142c52274d42cfa5
김주찬 <joockim@gimjuchan-ui-MacBookPro.local>
2021-09-03 21:42:26 +0900

    [Feat]: refresh data save (FT-81)
    새로고침 시 어플리케이션에 저장된 데이터들이 날라가는 현상 잡음
    redux-persist로 제어

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './modules';
+import configureStore from './modules';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(rootReducer);
+const { store, persistor } = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
--- front/src/modules/index.ts
+++ front/src/modules/index.ts
@@ -1,10 +1,30 @@
-import { combineReducers } from 'redux';
-import userModule from './profile';
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import {
+  combineReducers,
+  createStore,
+  Store,
+} from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import profileModule from './profile';
+import usermeModule from './userme';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
 const rootReducer = combineReducers({
-  userModule,
+  profileModule,
+  usermeModule,
 });
 
-export default rootReducer;
+const enhancedReducer = persistReducer(persistConfig, rootReducer);
+
+export default function configureStore() {
+  const store: Store = createStore(enhancedReducer);
+  const persistor = persistStore(store);
+  return { store, persistor };
+}
 
 export type RootState = ReturnType<typeof rootReducer>;
