fe8e4d748c22f940dc8cfedef5c36a150badb9f0
hyeonski <hyeonski@student.42seoul.kr>
2021-09-09 11:24:54 +0900

    [Fix]: Import AuthModule in Match for JwtAuthGuard
    jwtService가 인스턴스화되지 않는 문제 해결을 위해 MatchModule에
    AuthModule을 import

--- back/src/match/match.module.ts
+++ back/src/match/match.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 import { User } from 'src/users/entities/user.entity';
 import { Match } from './entities/match.entity';
 import { MatchController } from './match.controller';
 import { MatchService } from './match.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Match])],
+  imports: [TypeOrmModule.forFeature([User, Match]), AuthModule],
   controllers: [MatchController],
   providers: [MatchService],
   exports: [MatchService],
