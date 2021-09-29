5d741258bb50fa078ff2079594b1fa69f24d58dc
hyeonski <hyeonski@student.42seoul.kr>
2021-09-30 00:54:07 +0900

    [Etc]: Deleted db_test, socket.io-client

--- db_test/user.sql
+++
@@ -1,8 +0,0 @@
-insert into "user" (username, nickname, email, avatar) values ('hyeonski', 'hyeonski', 'hyeonski@student.42seoul.kr', 'http://cdn.intra.42.fr/users/hyeonski.jpg');
-insert into "user" (username, nickname, email, avatar) values ('jayun', 'jayun', 'jayun@student.42seoul.kr', 'http://cdn.intra.42.fr/users/jayun.jpg');
-insert into "user" (username, nickname, email, avatar) values ('juyang', 'juyang', 'juyang@student.42seoul.kr', 'http://cdn.intra.42.fr/users/juyang.jpg');
-insert into "user" (username, nickname, email, avatar) values ('joockim', 'joockim', 'joockim@student.42seoul.kr', 'http://cdn.intra.42.fr/users/joockim.jpg');
-insert into "user" (username, nickname, email, avatar) values ('sunpark', 'sunpark', 'sunpark@student.42seoul.kr', 'http://cdn.intra.42.fr/users/sunpark.jpg');
-insert into "user" (username, nickname, email, avatar) values ('amin', 'amin', 'amin@student.42seoul.kr', 'http://cdn.intra.42.fr/users/amin.jpg');
-insert into "user" (username, nickname, email, avatar) values ('dsohn', 'dsohn', 'dsohn@student.42seoul.kr', 'http://cdn.intra.42.fr/users/dsohn.jpg');
-insert into "user" (username, nickname, email, avatar) values ('jachoi', 'jachoi', 'jachoi@student.42seoul.kr', 'http://cdn.intra.42.fr/users/jachoi.jpg');
